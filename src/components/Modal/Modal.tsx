c5a7819762ce4ab848e7c2669c47a6b413e43260
Andreev Danila <mail@neandreev.ru>
2022-07-03 18:53:23 +0500

    update modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,16 +7,13 @@ const Modal: FC = () => {
   const data = useData();
 
   return (
-    <>
-      <div className={styles.darkBG}/>
-      <div className={styles.centered}>
-        <div className={styles.modal}>
-          <pre className={styles.modalContent}>
-            {JSON.stringify(data, null, 2)}
-          </pre>
-        </div>
+    <div className={styles.centered}>
+      <div className={styles.modal}>
+        <pre className={styles.modalContent}>
+          {JSON.stringify(data, null, 2)}
+        </pre>
       </div>
-    </>
+    </div>
   );
 };
 
